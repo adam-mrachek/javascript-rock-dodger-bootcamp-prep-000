63a70ab2ef0105e016578277f9f5a8068a21e7ae
Adam Mrachek <amrachek@outlook.com>
2018-04-10 03:13:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -135,9 +135,13 @@ function moveDodger(e) {
 
   document.addEventListener('keydown', function(e) {
     if (e.which === LEFT_ARROW) {
+      e.preventDefault();
+      e.stopPropagation();
       moveDodgerLeft();
     }
     if (e.which === RIGHT_ARROW) {
+      e.preventDefault();
+      e.stopPropagation();
       moveDodgerRight();
     }
   })
